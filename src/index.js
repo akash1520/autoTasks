bada42a92765152bedf16452f67c7380fdcb2643
akash1520 <akashparmar1520@gmail.com>
2023-09-02 20:12:59 +0530

    feat : vercel added

--- src/index.js
+++ src/index.js
@@ -8,6 +8,10 @@ const {router} = require("../routes/auth")
 const app = express();
 app.use(router)
 
+app.get('/', (req,res)=>{
+res.status(200).json({"message":"success"})
+})
+
 const PORT = process.env.PORT||3000 ;
 app.listen(PORT, () => {
   console.log(`Server started at http://localhost:${PORT}`);
